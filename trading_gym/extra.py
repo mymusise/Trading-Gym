65f6bb265f948c9aa39c9230c98b06db98b604f9
mymusise <mymusise1@gmail.com>
2019-02-14 01:43:02 -0500

    add math_hour feature
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- trading_gym/extra.py
+++ trading_gym/extra.py
@@ -1,5 +1,5 @@
 class ExtraFeature:
-    ex_obs_name = ['amount', 'buy_at', 'index', 'floating_rate']
+    ex_obs_name = ['amount', 'buy_at', 'index', 'floating_rate', 'math_hour']
 
     def get_extra_features(self, info):
         features = {}
@@ -7,5 +7,6 @@ class ExtraFeature:
         features['buy_at'] = info['buy_at'] / self.data.max_steps
         features['index'] = info['index'] / self.data.max_steps
         features['floating_rate'] = self.exchange.floating_rate
+        features['math_hour'] = self.obs.math_hour
 
         return [features[name] for name in self.ex_obs_name]
